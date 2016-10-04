966de0e17a202f387cede6821eed9ac8d1f019f9
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2016-10-04 23:30:22 +0900

    Solve Problem2

---
+++ problem2.py
@@ -0,0 +1,12 @@
+def ans():
+    ans = 0
+    x = 1
+    y = 2
+    while x <= 4000000:
+        if x % 2 == 0:
+            ans += x
+        x, y = y, x + y
+    return str(ans)
+
+if __name__ == '__main__':
+    print ans()
