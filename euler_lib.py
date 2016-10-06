66243f55f5c9488fd77e475af56c95b11bcb0447
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2016-10-06 11:00:49 +0900

    Modify my library file

--- euler_lib.py
+++ euler_lib.py
@@ -1,10 +1,6 @@
 # coding:utf8
 # 入力された整数nを素因数分解する
 def prime_factorization(n):
-    if n < 1:
-        return False
-    if n == 1:
-        return 1
     ans = []
     i = 2
     while i * i <= n:
@@ -14,6 +10,20 @@ def prime_factorization(n):
         i += 1
     if n > 1:
         ans.append(n)
+    return ans
+
+# 入力された整数nを素因数分解して辞書で返す
+# { key => 素因数, value => 素因数の個数 }
+def prime_factorization_for_dict(n):
+    ans = {}
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            n /= i
+            ans[i] = 1 if i not in ans else ans[i] + 1
+        i += 1
+    if n > 1:
+        ans[n] = 1 if n not in ans else ans[n] + 1
     return ans
 
 # 入力された整数が素数かどうか判定する
